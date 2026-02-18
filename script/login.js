4571dd38e62ccbac833bc1cef9c8bda06e345f71
salmansahed <salmansahed10@gmail.com>
2026-02-18 12:58:38 +0600

    fix broken link

--- script/login.js
+++ script/login.js
@@ -13,7 +13,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
         alert('Login Success');
 
         // window.location.replace('/home.html')
-        window.location.assign('/home.html')
+        window.location.assign('home.html')
     }
     else{
         //3-1 false:::>> alert> return
